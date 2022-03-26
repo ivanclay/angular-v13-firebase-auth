d695ab6f2888e1fa9beed5fe0d5d0a9fc1e0f1d1
ivanclay <ivan.clay.moura@gmail.com>
2022-03-26 20:32:17 -0300

    Angular + firestore CRUD

---
+++ Angular-Firebase-crud/src/environments/environment.ts
@@ -0,0 +1,15 @@
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "xxxxxxxx",
+    authDomain: "xxxxxxxx",
+    projectId: "xxxxxxxx",
+    storageBucket: "xxxxxxxx",
+    messagingSenderId: "xxxxxx",
+    appId: "xxxxxxxxxxx"
+  }
+};
+
+
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
